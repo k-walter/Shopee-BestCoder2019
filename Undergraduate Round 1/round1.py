6fcb302cf18ca0143185af55fada5f146bb1207e
Walter <walltail@users.noreply.github.com>
2019-10-21 02:22:39 +0800

    Code Submitted during Event
    Please refer to links on readme for data.

---
+++ Undergraduate Round 1/round1.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import re
+# from collections import defaultdict
+# import tensorflow as tf
+# import tensorflow.keras as K
+
+def n_gram(arr, n):
+    return zip(*(arr[_:] for _ in range(n)))
+
+d = dict()
+# dd = defaultdict(list)
+with open("Extra Material 2 - keyword list_with substring.csv") as f:
+	f.readline()
+	for line in f:
+		line = line.split(',')
+		grp, words = int(line[0]), line[1:]
+		for w in words:
+			w = w.strip('" \n').lower()
+			if w in d:
+				d[w] = min(d[w], grp)
+			else:
+				d[w] = grp
+
+out_file = "index,groups_found\n"
+with open("Keyword_spam_question.csv", encoding="utf8") as f:
+	f.readline()
+	for line in f:
+		line = line.split(',')
+		idx, line = int(line[0]), ''.join(line[1:])
+		line = re.sub(r'[^a-z0-9]', ' ', line.strip().lower())
+		line = re.sub(r'[ ]+', ' ', line.strip())
+
+		arr = line.split(' ')
+		print(idx)
+		# print(arr)
+
+		length = len(arr)
+		out = set()
+		while length > 0:
+			# print(f"Length {length}")
+			for gram in n_gram(arr, length):
+				key = ' '.join(gram)
+				if key in d:
+					out.add(d[key])
+					line = line.replace(key, '')
+					line = re.sub(r'[ ]+', ' ', line)
+					arr = line.split(' ')
+					# print(key, arr)
+			length -= 1
+			if len(arr) < length:
+				length = len(arr)
+		# print(sorted(out))
+		if len(out) == 1:
+			out_file += f'{idx},{list(out)}\n'
+		else:
+			out_file += f'{idx},"{sorted(out)}"\n'
+		# break
+
+with open("out.csv", 'w', encoding="utf8") as f:
+	f.write(out_file)
+print("done")
---
+++ Undergraduate Round 2/round2.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+from itertools import chain
+from collections import defaultdict, deque
+
+usr_ba = defaultdict(set)
+ba_usr = defaultdict(set)
+with open("bank_accounts.csv",) as f:
+	f.readline()
+	for line in f:
+		line = line.strip().split(',')
+		user, bank = line
+		ba_usr[bank].add(user)
+		usr_ba[user].add(bank)
+
+usr_cc = defaultdict(set)
+cc_usr = defaultdict(set)
+with open("credit_cards.csv",) as f:
+	f.readline()
+	for line in f:
+		line = line.strip().split(',')
+		user, bank = line[0], line[1]
+		cc_usr[bank].add(user)
+		usr_cc[user].add(bank)
+
+usr_d = defaultdict(set)
+d_usr = defaultdict(set)
+with open("devices.csv",) as f:
+	f.readline()
+	for line in f:
+		line = line.strip().split(',')
+		user, bank = line[0], line[1]
+		d_usr[bank].add(user)
+		usr_d[user].add(bank)
+
+def isfraud(user,sell,usr_dd,dd_usr,visited,q):
+	for ba in usr_dd[user]:
+		for usr in dd_usr[ba]:
+			if usr == sell:
+				return True
+			if usr not in visited:
+				q.append(usr)
+				visited.add(usr)
+	return False
+
+out_file = "orderid,is_fraud\n"
+with open("orders.csv") as f:
+	f.readline()
+	for line in f:
+		line = line.strip().split(',')
+		order, buy, sell = line
+
+		q = deque()
+		q.append(buy)
+		visited = {buy}
+
+		fraud = False
+		out_file += order + ","
+		while len(q) > 0:
+			user = q.popleft()
+			if isfraud(user,sell,usr_ba,ba_usr,visited,q) or isfraud(user,sell,usr_cc,cc_usr,visited,q) or isfraud(user,sell,usr_d,d_usr,visited,q):
+				# print(buy,sell,"\n")
+				fraud = True
+				break
+
+		out_file += "1" if fraud else "0"
+		out_file += "\n"
+		# break
+
+with open("out.csv", "w", encoding="utf8") as f:
+	f.write(out_file)
+print("done")
